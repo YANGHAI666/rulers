20c72ce50f3662a79df2073635115f49940fa74f
xiongxiong <35870702+YANGHAI666@users.noreply.github.com>
2022-09-01 23:14:17 +0800

    Update video.js
    1

--- video.js
+++ video.js
@@ -1,10 +1,8 @@
-
-var body = $response.body;
-
+var body = $response.body; 
 
 var obj = JSON.parse($response.body); 
 
 
-obj = {"response":[{"regions":{},"variantDebugInfo":{"abTests":[{"id":5652427186438144,"variantId":6118677561802752,"vars":{"vl_template_editor_experiment_05_2022":"EVDTemplateEditorExperimentVariant1"}},{"id":5227014022823936,"variantId":6133509249892352,"vars":{"highlight_feature_experiment_v_1_19_1":"EVDHighlightExperimentEnabled"}}]},"success":true,"messages":{},"vars":{"vla_system_trial_run_experiment_v2":"Baseline","vl_template_editor_experiment_05_2022":"EVDTemplateEditorExperimentVariant1","highlight_feature_experiment_v_1_19_1":"EVDHighlightExperimentEnabled"},"variants":[{"name":"Overrides","id":6118677561802752,"abTestName":"Template Editor Activation","abTestId":5652427186438144},{"name":"Overrides","id":6133509249892352,"abTestName":"Highlight Tool Activation","abTestId":5227014022823936}],"syncNewsfeed":false,"localCaps":[],"token":"Jsde52WlRnGipPLPudCJfBlAPt92gUyYlpUq1ty7pyE","reqId":"2342D511-D686-4D2D-96A8-3208757E9002"}]}
+obj = {"latestProductId":"com.lightricks.EnlightVideo_V2.PQ.1Y.SA_1Y.SA_TRIAL.1W","latestPurchaseDateMs":1662043889000,"originalPurchaseDateMs":1662043890000,"originalTransactionId":"210001177415261","pendingRenewalInfo":{"nextProductId":"com.lightricks.EnlightVideo_V2.PQ.1Y.SA_1Y.SA_TRIAL.1W","isAutoRenewEnabled":true,"expirationIntent":null},"isExpired":false,"latestTransactionId":"210001177415261","latestExpirationDateMs":3871723889000,"fullRefundDateMs":null}
 
 $done({body:JSON.stringify(obj)});
